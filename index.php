3989bf7d064e082aad75c784f43187f5c713cbf4
Gabriel Costa de Souza <gabrielsouza_1909@hotmail.com>
2024-07-26 09:48:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Conteúdo | Lógica Digital</title>
+
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" type="image/x-icon" href="./trafego-pago/assets/img/favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+</head>
+<body>
+  <nav class="navbar navbar-expand-md bg-dark sticky-top border-bottom" data-bs-theme="dark" style="">
+    <div class="container">
+      <a class="navbar-brand d-md-none" href="https://www.logicadigital.com.br">
+        <img src="./trafego-pago/assets/img/favicon.png" alt="Logo Agência Lógica Digital" class="bi" width="100">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasLabel">Aperture</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav flex-grow-1 justify-content-between align-items-center">
+            <li class="nav-item"><a class="nav-link" href="https://www.logicadigital.com.br">
+              <img src="./trafego-pago/assets/img/favicon.png" alt="Logo Agência Lógica Digital" width="70">
+            </a></li>
+            <li class="nav-item"><a class="nav-link" href="https://www.logicadigital.com.br">Lógica Digital</a></li>
+            <li class="nav-item"><a class="nav-link" href="#" onclick="myFunction()">Criação</a></li>
+            <li class="nav-item"><a class="nav-link" href="#" onclick="myFunction()">Desenvolvimento</a></li>
+            <li class="nav-item"><a class="nav-link" href="#" onclick="myFunction()">Marketing</a></li>
+            <li class="nav-item"><a class="nav-link" href="./trafego-pago/">Trafego Pago</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <main>
+    <div class="container marketing">
+      <div class="row">
+        <div class="col-lg-6">
+        <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" viewBox="0 0 24 24"><path fill="white" d="M4.616 21q-.691 0-1.153-.462T3 19.385V8.615q0-.69.463-1.152T4.615 7h4.27L12 3.885L15.116 7h4.269q.69 0 1.153.463T21 8.616v10.769q0 .69-.462 1.153T19.385 21zm0-1h14.769q.23 0 .423-.192t.192-.424V8.616q0-.231-.192-.424T19.385 8H4.615q-.23 0-.423.192T4 8.616v10.769q0 .23.192.423t.423.192M7.5 17.5h9.154l-2.827-3.77l-2.615 3.308l-1.75-2.115zm9.423-4.5q.471 0 .793-.322t.323-.793t-.323-.794t-.793-.322t-.793.322t-.322.793t.322.794t.793.322m-6.784-6h3.723L12 5.139zM4 20V8z"/></svg>
+          <h2 class="fw-normal">Criação</h2>
+          <p>A identidade visual da sua marca é o ponto de partida para sua comunicação. Definimos padrões visuais que serão aplicados no logotipo e outros materiais institucionais que compõem a comunicação visual da empresa.</p>
+          <p><a class="btn btn-secondary" onclick="myFunction()" href="#">Ver mmais detalhes »</a></p>
+        </div>
+        <div class="col-lg-6">
+        <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" viewBox="0 0 24 24"><path fill="white" d="M1.77 19.462v-1h20.46v1zm2.846-2q-.691 0-1.153-.463T3 15.846V6.077q0-.69.463-1.153t1.152-.462h14.77q.69 0 1.152.462T21 6.077v9.77q0 .69-.463 1.152t-1.153.463zm0-1h14.769q.23 0 .423-.193t.192-.423V6.077q0-.23-.192-.423q-.193-.193-.424-.193H4.616q-.231 0-.424.193Q4 5.846 4 6.077v9.77q0 .23.192.422t.423.193m-.615 0v-11z"/></svg>
+          <h2 class="fw-normal">Desenvolvimento</h2>
+          <p>Desenvolvemos sites exclusivos que atendem seus objetivos e necessidades. Apresente sua empresa, produtos e serviços na web e aumente o número de contatos e vendas. Nós temos a solução ideal!</p>
+          <p><a class="btn btn-secondary" onclick="myFunction()" href="#">Ver mmais detalhes »</a></p>
+        </div>
+        <div class="col-lg-6">
+        <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" viewBox="0 0 24 24"><path fill="white" d="M11.412 16.962q-1.881-.24-3.147-1.639Q7 13.925 7 12q0-2.083 1.458-3.542Q9.917 7 12 7q1.925 0 3.323 1.256q1.398 1.255 1.639 3.136l-1.081-.317q-.324-1.35-1.393-2.212Q13.418 8 12 8q-1.65 0-2.825 1.175T8 12q0 1.425.872 2.5t2.203 1.4zm1.2 4.007q-.153.031-.306.031H12q-1.868 0-3.51-.708t-2.857-1.924t-1.924-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709t2.859 1.924t1.925 2.857T21 12v.296q0 .148-.03.296L20 12.3V12q0-3.35-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20h.3zm7.144-.18l-4.794-4.814l-.923 2.794L12 12l6.77 2.039l-2.795.923l4.814 4.794z"/></svg>
+          <h2 class="fw-normal">Marketing</h2>
+          <p>Certificada como Agência Digital Parceira do Google e com certificação de Inbound Marketing RD Station, buscamos atualizações constantes no mundo cada vez mais conectado e utilizamos as mais novas ferramentas e tecnologias disponíveis no mercado.</p>
+          <p><a class="btn btn-secondary" onclick="myFunction()" href="#">Ver mmais detalhes »</a></p>
+        </div>
+        <div class="col-lg-6">
+        <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" viewBox="0 0 24 24"><path fill="white" d="M3 19V5h18v14zm1-1h16V6H4zm0 0V6zm4.5-1.5h1v-1h1.23q.328 0 .549-.221t.221-.548v-2.462q0-.327-.221-.548t-.548-.221H7.5v-2h4v-1h-2v-1h-1v1H7.27q-.328 0-.549.221T6.5 9.27v2.462q0 .327.221.548t.548.221H10.5v2h-4v1h2zm7.5-.52l1.539-1.538H14.46zm-1.538-6.172h3.077L16 8.269z"/></svg>
+          <h2 class="fw-normal">Trafego Pago</h2>
+          <p>Campanhas de Mídia Digital aumentam o conhecimento e lembrança de marca em plataformas digitais e geram mais conversões para sua empresa: aumento no número de contatos, inscrições e vendas.</p>
+          <p><a class="btn btn-secondary" href="./trafego-pago/">Ver mmais detalhes »</a></p>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <span class="mb-3 mb-md-0">&copy; <?php echo date('Y'); ?> Todos os direitos reservados.</span>
+      </div>
+
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <a href="https://www.logicadigital.com.br" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
+          Desenvolvido pela Lógica Digital <img src="./trafego-pago/assets/img/favicon.png" alt="Logo Agência Lógica Digital" width="50">
+        </a>
+      </ul>
+    </footer>
+  </div>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <a href="https://wa.me/5519982394436?text=Ol%C3%A1%2C+L%C3%B3gica+Digital%21+Quero+contratar+um+serviço" style="position:fixed;width:60px;height:60px;bottom:40px;left:40px;background-color:#25d366;color:#FFF;border-radius:50px;text-align:center;font-size:30px;box-shadow: 1px 1px 2px #888;
+    z-index:1000;" target="_blank">
+    <i style="margin-top:16px" class="fa fa-whatsapp"></i>
+  </a>
+
+  <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
+
+  <script>
+    function myFunction() {
+      alert("Página em Desenvolvimento!");
+    }
+  </script>
+  
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
